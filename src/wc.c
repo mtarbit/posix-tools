3b365ddd582255c8cca7614452ec4a2d8bdeaf59
Matt Tarbit <mtarbit@gmail.com>
2013-04-12 14:11:17 +0100

    Support options in wc.

--- src/wc.c
+++ src/wc.c
@@ -2,13 +2,36 @@
 #include <limits.h>
 #include <locale.h>
 #include <ctype.h>
+#include <getopt.h>
 
 // For -m flag see discussion of multi-byte / wide-char handling here:
 // http://stackoverflow.com/questions/14083706/iterating-through-a-char-array-with-non-standard-chars/14083757#14083757
 
+int l_option = 0,
+    w_option = 0,
+    m_option = 0,
+    c_option = 0;
+
+void usage(const char *program_name) {
+    die_msg("Usage: %s [-lwmc]... [FILE]...", program_name);
+}
+
+void print_counts(int l, int w, int m, int c, const char *label) {
+    if (l_option || w_option || m_option || c_option) {
+        if (l_option) printf("%d ", l);
+        if (w_option) printf("%d ", w);
+        if (m_option) printf("%d ", m);
+        if (c_option) printf("%d ", c);
+        puts(label);
+    } else {
+        printf("%d %d %d %s\n", l, w, c, label);
+    }
+}
+
 int main(int argc, char *argv[]) {
     int i, j;
     int len;
+    int opt;
 
     int l_count,
         w_count,
@@ -20,15 +43,31 @@ int main(int argc, char *argv[]) {
         m_total = 0,
         c_total = 0;
 
+    int filec;
+    char **filev;
+
     FILE *fp;
     char buf[LINE_MAX];
     char curr_chr;
     char last_chr;
 
+    while ((opt = getopt(argc, argv, "lwmc")) != -1) {
+        switch (opt) {
+            case 'l': l_option = 1; break;
+            case 'w': w_option = 1; break;
+            case 'm': m_option = 1; break;
+            case 'c': c_option = 1; break;
+            default:  usage(argv[0]);
+        }
+    }
+
+    filec = argc - optind;
+    filev = &argv[optind];
+
     setlocale(LC_ALL, "");
 
-    for (i = 1; i < argc; i++) {
-        if ((fp = fopen(argv[i], "r")) == NULL) {
+    for (i = 0; i < filec; i++) {
+        if ((fp = fopen(filev[i], "r")) == NULL) {
             die("fopen");
         }
 
@@ -41,8 +80,6 @@ int main(int argc, char *argv[]) {
             for (j = 0; buf[j] && (len = mblen(&buf[j], LINE_MAX - j)) > 0; j += len) {
                 c_count += len;
                 m_count++;
-
-                // printf("%d: '%.*s', %d\n", m_count, len, &buf[j], len);
 
                 curr_chr = buf[j];
 
@@ -63,11 +100,11 @@ int main(int argc, char *argv[]) {
         m_total += m_count;
         c_total += c_count;
 
-        printf("%d %d %d %s\n", l_count, w_count, c_count, argv[i]);
+        print_counts(l_count, w_count, m_count, c_count, filev[i]);
     }
 
-    if (argc > 2) {
-        printf("%d %d %d %s\n", l_total, w_total, c_total, "total");
+    if (filec > 2) {
+        print_counts(l_total, w_total, m_total, c_total, "total");
     }
 
     exit(EXIT_SUCCESS);
